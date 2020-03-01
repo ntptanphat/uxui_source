5e015a2e7ae7423f4a4fef2bb720fc6601c08b90
Phat Nguyen <ntptanphat@gmail.com>
2020-03-01 16:39:55 +0700

    add typeit jquery

---
+++ js/typeit.js
@@ -0,0 +1,21 @@
+new TypeIt("#subTitleTypeit", {
+    strings: "AVENTADOR SVG",
+    speed: 75,
+    startDelay: 2300,
+    cursorChar: '',
+    loop: true,
+    loopDelay: 39000,
+    deleteSpeed: 0
+  }, function() {
+    $('.ti-cursor').remove();
+  }).go();
+
+  new TypeIt("#titleTypeit", {
+    strings: "REAL EMOTIONS SHAPE THE FUTURE",
+    speed: 40,
+    startDelay: 3500,
+    cursorChar: '',
+    loop: true,
+    loopDelay: 39000,
+    deleteSpeed: 0
+  }).go();
