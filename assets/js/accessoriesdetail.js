5812a47b1187ea83fa2601d3a7a8c30134955643
Phat Nguyen <ntptanphat@gmail.com>
2020-03-18 23:21:07 +0700

    done accessories detail

--- assets/js/accessoriesdetail.js
+++ assets/js/accessoriesdetail.js
@@ -1,47 +1,212 @@
-const accessoriesData = [
+const accessoriesLightsData = [
     {
         id: 1,
-        title: 'RTX, Steel Rim',
-        imgSrc: 'assets/images/accessories/accessories_item_1.jpg',
-        description: "PLEASE READ ENTIRE LISTING - Rim / Wheel size: 16X6.5 Bolt Pattern: 5X114.3 Center Bore: 71.5"
+        title: 'Light 1',
+        imgSrc: 'assets/images/accessories/lights/1.jpg',
+        description: "Content 1"
     },
     {
         id: 2,
-        title: 'American Racing AR172',
-        imgSrc: 'assets/images/accessories/accessories_item_2.jpg',
-        description: 'Original Equipment (OE) Number: 88974912, 4261101180, 4261101181, 4261102A00, 4261112B80'
+        title: 'Light 2',
+        imgSrc: 'assets/images/accessories/lights/2.jpg',
+        description: 'Content 2'
     },
     {
         id: 3,
-        title: 'Dorman 939-174',
-        imgSrc: 'assets/images/accessories/accessories_item_3.jpg',
-        description: 'Direct fit replacement to ensure proper wheel cover fit <br/>Rigorous inspection has been undertaken to ensure high quality'
+        title: 'Light 3',
+        imgSrc: 'assets/images/accessories/lights/3.jpg',
+        description: 'Content 3'
     },
     {
         id: 4,
-        title: 'Road Ready Car',
-        imgSrc: 'assets/images/accessories/accessories_item_4.jpg',
-        description: 'PRECISION MADE PRICED RIGHT - Road Ready focuses on specific manufacturer design for a perfect car rim replacement. ✓ Purchasing one (1) new steel wheel ✓ That Fits : 2009 2010'
+        title: 'Light 4',
+        imgSrc: 'assets/images/accessories/lights/4.jpg',
+        description: 'Content 4'
     },
     {
         id: 5,
-        title: 'Konig Illusion Black',
-        imgSrc: 'assets/images/accessories/accessories_item_5.jpg',
-        description: 'Fully compatible with original wheel covers and all TPMS sensors'
+        title: 'Light 5',
+        imgSrc: 'assets/images/accessories/lights/5.jpg',
+        description: 'Content 5'
     },
     {
         id: 6,
-        title: 'RDR Wheels CBRD1',
-        imgSrc: 'assets/images/accessories/accessories_item_6.jpg',
-        description: 'FITS ALL RIM REPLACEMENT NEEDS - Road Ready OEM replacement wheels are perfect for single wheel replacements, full set swaps, or spare rim needs. Designed to stash conveniently in your trunk for times when you lose a rim while out on the road. *Note: This purchase DOES NOT include TPMS sensors, lug nuts, valve stems, or wheel covers.'
+        title: 'Light 6',
+        imgSrc: 'assets/images/accessories/lights/6.jpg',
+        description: '6'
+    },
+    {
+        id: 7,
+        title: 'Light 7',
+        imgSrc: 'assets/images/accessories/lights/7.jpg',
+        description: 'Content 7'
+    },
+    {
+        id: 8,
+        title: 'Light 8',
+        imgSrc: 'assets/images/accessories/lights/8.jpg',
+        description: 'Content 7'
     }
-]
-$('.accessoriesdetail-slide').on('click', '.item', function (event) {
+];
+
+const accessoriesWheelData = [
+    {
+        id: 1,
+        title: 'Wheel Title 1',
+        imgSrc: 'assets/images/accessories/wheel/1.jpg',
+        description: "Content 1"
+    },
+    {
+        id: 2,
+        title: 'Wheel Title 2',
+        imgSrc: 'assets/images/accessories/wheel/2.jpg',
+        description: 'Content 2'
+    },
+    {
+        id: 3,
+        title: 'Wheel Title 3',
+        imgSrc: 'assets/images/accessories/wheel/3.jpg',
+        description: 'Content 3'
+    },
+    {
+        id: 4,
+        title: 'Wheel Title 4',
+        imgSrc: 'assets/images/accessories/wheel/4.jpg',
+        description: 'Content 4'
+    },
+    {
+        id: 5,
+        title: 'Wheel Title 5',
+        imgSrc: 'assets/images/accessories/wheel/5.jpg',
+        description: 'Content 5'
+    },
+    {
+        id: 6,
+        title: 'Wheel Title 6',
+        imgSrc: 'assets/images/accessories/wheel/6.jpg',
+        description: 'Content 6'
+    }
+];
+
+const accessoriesSeatData = [
+    {
+        id: 1,
+        title: 'Seat Title 1',
+        imgSrc: 'assets/images/accessories/seat/1.jpg',
+        description: "Content 1"
+    },
+    {
+        id: 2,
+        title: 'Seat Title 2',
+        imgSrc: 'assets/images/accessories/seat/2.jpg',
+        description: 'Content 2'
+    },
+    {
+        id: 3,
+        title: 'Seat Title 3',
+        imgSrc: 'assets/images/accessories/seat/3.jpg',
+        description: 'Content 3'
+    },
+    {
+        id: 4,
+        title: 'Seat Title 4',
+        imgSrc: 'assets/images/accessories/seat/4.jpg',
+        description: 'Content 4'
+    },
+    {
+        id: 5,
+        title: 'Seat Title 5',
+        imgSrc: 'assets/images/accessories/seat/5.jpg',
+        description: 'Content 5'
+    },
+    {
+        id: 6,
+        title: 'Seat Title 6',
+        imgSrc: 'assets/images/accessories/lights/6.jpg',
+        description: 'Content 6'
+    }
+];
+
+const accessoriesSteeringWheelData = [
+    {
+        id: 1,
+        title: 'SteeringWheel Title 1',
+        imgSrc: 'assets/images/accessories/steering_wheel/1.jpg',
+        description: "Content 1"
+    },
+    {
+        id: 2,
+        title: 'SteeringWheel Title 2',
+        imgSrc: 'assets/images/accessories/steering_wheel/2.jpg',
+        description: 'Content 2'
+    },
+    {
+        id: 3,
+        title: 'SteeringWheel Title 3',
+        imgSrc: 'assets/images/accessories/steering_wheel/3.jpg',
+        description: 'Content 3'
+    },
+    {
+        id: 4,
+        title: 'SteeringWheel Title 4',
+        imgSrc: 'assets/images/accessories/steering_wheel/4.jpg',
+        description: 'Content 4'
+    },
+    {
+        id: 5,
+        title: 'SteeringWheel Title 5',
+        imgSrc: 'assets/images/accessories/steering_wheel/5.jpg',
+        description: 'Content 5'
+    },
+    {
+        id: 6,
+        title: 'SteeringWheel Title 6',
+        imgSrc: 'assets/images/accessories/steering_wheel/6.jpg',
+        description: 'Content 6'
+    }
+];
+$('.accessoriesdetail-lights .accessoriesdetail-slide').on('click', '.item', function (event) {
     const accessoriesId = $(this).data("id");
     const accessoriesTitleEl = $('#accessoriesTitle');
     const accessoriesDescriptionEl = $('#accessoriesDescription');
     const accessoriesImageSrcEl = $('#accessoriesImageSrc');
-    const accessoriesActive = accessoriesData.find(item => item.id == accessoriesId);
+    const accessoriesActive = accessoriesLightsData.find(item => item.id == accessoriesId);
+    // console.log(accessoriesData.find(item => item.id == accessoriesId));
+    accessoriesTitleEl.html(accessoriesActive.title);
+    accessoriesDescriptionEl.html(accessoriesActive.description);
+    accessoriesImageSrcEl.attr('src', accessoriesActive.imgSrc);
+})
+
+$('.accessoriesdetail-wheel .accessoriesdetail-slide').on('click', '.item', function (event) {
+    const accessoriesId = $(this).data("id");
+    const accessoriesTitleEl = $('#accessoriesTitle');
+    const accessoriesDescriptionEl = $('#accessoriesDescription');
+    const accessoriesImageSrcEl = $('#accessoriesImageSrc');
+    const accessoriesActive = accessoriesWheelData.find(item => item.id == accessoriesId);
+    // console.log(accessoriesData.find(item => item.id == accessoriesId));
+    accessoriesTitleEl.html(accessoriesActive.title);
+    accessoriesDescriptionEl.html(accessoriesActive.description);
+    accessoriesImageSrcEl.attr('src', accessoriesActive.imgSrc);
+})
+
+$('.accessoriesdetail-seat .accessoriesdetail-slide').on('click', '.item', function (event) {
+    const accessoriesId = $(this).data("id");
+    const accessoriesTitleEl = $('#accessoriesTitle');
+    const accessoriesDescriptionEl = $('#accessoriesDescription');
+    const accessoriesImageSrcEl = $('#accessoriesImageSrc');
+    const accessoriesActive = accessoriesSeatData.find(item => item.id == accessoriesId);
+    // console.log(accessoriesData.find(item => item.id == accessoriesId));
+    accessoriesTitleEl.html(accessoriesActive.title);
+    accessoriesDescriptionEl.html(accessoriesActive.description);
+    accessoriesImageSrcEl.attr('src', accessoriesActive.imgSrc);
+})
+
+$('.accessoriesdetail-steeringwheel .accessoriesdetail-slide').on('click', '.item', function (event) {
+    const accessoriesId = $(this).data("id");
+    const accessoriesTitleEl = $('#accessoriesTitle');
+    const accessoriesDescriptionEl = $('#accessoriesDescription');
+    const accessoriesImageSrcEl = $('#accessoriesImageSrc');
+    const accessoriesActive = accessoriesSteeringWheelData.find(item => item.id == accessoriesId);
     // console.log(accessoriesData.find(item => item.id == accessoriesId));
     accessoriesTitleEl.html(accessoriesActive.title);
     accessoriesDescriptionEl.html(accessoriesActive.description);
