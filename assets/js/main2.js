992a9f9b1b1273f662f83616bda39640af63a545
Phat Nguyen <ntptanphat@gmail.com>
2020-03-22 15:29:17 +0700

    fix bug carousel fullpage

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -69,5 +69,14 @@
 		}, 600);
 		return false;
 	});
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 
 })(this.jQuery);
