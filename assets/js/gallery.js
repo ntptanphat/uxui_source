92d8fefc92886635d6aa57b2d5351e7738680b8c
Phat Nguyen <ntptanphat@gmail.com>
2020-03-25 02:40:20 +0700

    fix bug

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -15,41 +15,5 @@
         }
 		lastScroll = currentScroll;        
 	};
-		$(window).load(function(){
-			$('.section-is-content-wp').isotope({
-				// options
-				itemSelector: '.isotopeItem',
-				layoutMode: 'fitRows'
-			});
-		});	
-  
-	  var $container = $('.section-is-content-wp');
-	  $(document).on('click','.filter-tab a',function(){
-		// removeClass a current
-		  $('.filter-tab a').removeClass('current');
-		  $('.filter-tab ').removeClass('filter-active');
-		  // Add class a current
-		  $(this).addClass('current');       
-		  $(this).parents(".filter-tab").addClass('filter-active');
-		  // Get text when choose item
-		  $(this).parents(".filter-section").children(".show-tabs-mobile").text($(this).text());
-		  // Remove menu filter in mobile
-		  // if (screen.width <= 768) {
-		  //     $(this).parents(".filter-section").removeClass("open-tab-filter");
-		  //     new WOW().init();
-		  // }
-  
-		  // filter
-		  var selector = $(this).attr('data-filter');
-		  $container.isotope({
-			  filter: selector,
-			  animationOptions: {
-				  duration: 1000,
-				  easing: 'easeOutQuart',
-				  queue: false
-			  }
-		  });
-		  return false;
-	  });
 
 })(this.jQuery);
