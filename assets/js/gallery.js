90f9705070c84c0d290a2c4106eb940c5da0f894
Phat Nguyen <ntptanphat@gmail.com>
2020-03-28 16:52:41 +0700

    fix image width

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,9 +1,18 @@
 (function ($) {
 	var lastScroll = 0;
 	const $navigation = $('.gallery-navigation');
+	const $panelFilter = $('.panel-filter');
 	window.onscroll = function() {
 		let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-
+		if(currentScroll > 100) {
+			if(!$panelFilter.hasClass('fixed')) {
+				$panelFilter.addClass('fixed');
+			}
+		} else {
+			if($panelFilter.hasClass('fixed')) {
+				$panelFilter.removeClass('fixed');
+			}
+		}
 		if (lastScroll <= currentScroll){
 			if($navigation.hasClass('is-show')) {
 				$navigation.removeClass("is-show");  
